c4cf00282a28226feecc8aed0577e3ca2e0b0d3c
Temanbelaj <91354884+Temanbelaj@users.noreply.github.com>
2021-11-06 10:51:54 +0700

    Add files via upload

---
+++ farhat.py
@@ -0,0 +1,29 @@
+print(" Slip Gaji PY. XYZ ")
+print("-------------------")
+# Ahmad
+nama = "Ahmad"
+agama = "Islam"
+anak = 2
+gajiahmad = '4000000'
+
+
+tunjanganjabatan: 0.2 * gajiahmad
+tunjangankeluarga =
+if anak <= 2:
+    tunjangan: 0.1 * gajiahmad
+elif anak > 2:
+    tunjangan_keluarga: 0.2 * gajiahmad
+else:
+    ("belum dapat tunjangan")
+Gajikotor = gajiahmad + tunjanganjabatan + tunjangan
+Zakatprofesi = (0, 0.025)[
+    agama == "Islam" and Gajikotor >= 6000000
+] * Gajikotor
+Gajibersih = Gajikotor - Zakatprofesi
+
+
+print("Nama Karyawan                \t:" ,nama,
+      "Agama                        \t:" ,agama
+      "Gaji                         \t:" ,gajiahmad
+)
+
