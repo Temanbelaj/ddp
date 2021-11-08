f3a212d34f90dc7719abd4eadb473145bcc20472
Temanbelaj <91354884+Temanbelaj@users.noreply.github.com>
2021-11-09 05:19:28 +0700

    Add files via upload

--- farhat.py
+++ farhat.py
@@ -1,29 +1,53 @@
 print(" Slip Gaji PY. XYZ ")
 print("-------------------")
-# Ahmad
-nama = "Ahmad"
+
+Nama = "Ahmad"
+    print ("Nama         \t:",Nama,)
 agama = "Islam"
+    print ("Agama        \t:",agama)
 anak = 2
-gajiahmad = '4000000'
+    print ("Jumlah Anan  \t:",anak,)
+gaji = 4000000
+    print ("Gaji Pokok       \t:",gaji,)
 
+tunjangan: 0.2 * gaji
+    print ("Tunjangan Jabatan       \t:",tunjangan,)
 
-tunjanganjabatan: 0.2 * gajiahmad
-tunjangankeluarga =
-if anak <= 2:
-    tunjangan: 0.1 * gajiahmad
-elif anak > 2:
-    tunjangan_keluarga: 0.2 * gajiahmad
+if (anak) <= 2:
+    print(0.1 * gaji)
+elif (anak) > 2:
+    print(0.2 * gaji)
 else:
-    ("belum dapat tunjangan")
-Gajikotor = gajiahmad + tunjanganjabatan + tunjangan
-Zakatprofesi = (0, 0.025)[
-    agama == "Islam" and Gajikotor >= 6000000
-] * Gajikotor
-Gajibersih = Gajikotor - Zakatprofesi
+    print(0)
+gaji1: 4000000 + 0.2 * gaji + anak
+    print ("Gaji Kotor          \t:",gaji1,)
+zakat: (0, 0.025)[agama == "Islam" and 4000000 + 0.2 * gaji + anak >= 6000000] * 4000000 + 0.2 * gaji + anak
+    print ("Zakat Profesi           \t:",zakat,)
+gaji2 = 4000000 + 0.2 * gaji + anak -  (0, 0.025)-[agama == "Islam" and 4000000 + 0.2 * gaji + anak >= 6000000
 
+print(" Slip Gaji PY. XYZ ")
+print("-------------------")
 
-print("Nama Karyawan                \t:" ,nama,
-      "Agama                        \t:" ,agama
-      "Gaji                         \t:" ,gajiahmad
-)
+Nama2 = "Alex"
+    print ("Nama         \t:",Nama2,)
+agama2 = "Islam"
+    print ("Agama        \t:",agama2)
+anak2 = 2
+    print ("Jumlah Anan  \t:",anak2,)
+gaji3 = 4000000
+    print ("Gaji Pokok       \t:",gaji3,)
+
+tunjangan: 0.2 * gaji3
+    print ("Tunjangan Jabatan       \t:",tunjangan,)
+
+if (anak) <= 2:
+    print(0.1 * gaji)
+elif (anak) > 2:
+    print(0.2 * gaji)
+else:
+    print(0)
+gaji1: 4000000 + 0.2 * gaji + anak
+    print ("Gaji Kotor          \t:",gaji1,)
+
+gaji2 = 4000000 + 0.2 * gaji + anak -  (0, 0.025)-[agama == "Islam" and 4000000 + 0.2 * gaji + anak >= 6000000
 
